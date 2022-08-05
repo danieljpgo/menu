e4563334cd4a5b2b5da5f383041859ca2482b833
Daniel Jorge <djpgonzaga@gmail.com>
2022-08-05 02:17:03 -0300

    chore: seed db

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,44 +1,61 @@
+// const { ingredients } = require("./ingredients");
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcryptjs");
+
+const ingredients = [
+  {
+    name: "Batata Inglesa",
+    unit: "g",
+  },
+];
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
-
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
+  const seedUser = await prisma.user.findUnique({
+    where: { email: "rachel@remix.run" },
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
+  if (!seedUser) {
+    const email = "rachel@remix.run";
+    const hashedPassword = await bcrypt.hash("racheliscool", 10);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: {
+          create: {
+            hash: hashedPassword,
+          },
         },
       },
-    },
-  });
+    });
+    await prisma.note.create({
+      data: {
+        title: "My first note",
+        body: "Hello, world!",
+        userId: user.id,
+      },
+    });
+    await prisma.note.create({
+      data: {
+        title: "My second note",
+        body: "Hello, world!",
+        userId: user.id,
+      },
+    });
+  }
 
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
+  const currentIngredients = await prisma.ingredient.findMany();
+  await Promise.all(
+    currentIngredients.map((ingredient) =>
+      prisma.ingredient.delete({ where: { id: ingredient.id } })
+    )
+  );
+  await Promise.all(
+    ingredients.map((ingredient) =>
+      prisma.ingredient.create({ data: ingredient })
+    )
+  );
 
   console.log(`Database has been seeded. 🌱`);
 }
