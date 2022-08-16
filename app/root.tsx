031f1b1ba1ca7d5a7c67c3e4777faf67c339b1e0
Daniel Jorge <djpgonzaga@gmail.com>
2022-08-16 01:10:16 -0300

    feat: generic erros pages

--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
-
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./server/session.server";
 
@@ -18,8 +18,8 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
   viewport: "width=device-width,initial-scale=1",
+  // title: "Remix Notes",
 });
 
 export async function loader({ request }: LoaderArgs) {
@@ -28,19 +28,72 @@ export async function loader({ request }: LoaderArgs) {
   });
 }
 
+// @TODO check if pages title withs ok
+
 export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+    // <html lang="en" className="h-full">
+    //   <head>
+    //     <Meta />
+    //     <Links />
+    //   </head>
+    //   <body className="h-full">
+    //     <ScrollRestoration />
+    //     <Scripts />
+    //     <LiveReload />
+    //   </body>
+    // </html>
+  );
+}
+
+type DocumentProps = {
+  children: React.ReactNode;
+  title?: string;
+};
+function Document(props: DocumentProps) {
+  const { children, title = "Untitled" } = props;
+
   return (
     <html lang="en" className="h-full">
       <head>
         <Meta />
+        <title>{title}</title>
         <Links />
       </head>
       <body className="h-full">
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <Document title="Uh-oh!">
+      <main>
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </main>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <main>
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+      </main>
+    </Document>
   );
 }
