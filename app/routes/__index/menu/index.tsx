82dafb8b2b09413ebbb65720ce1666b5ee4d4da4
Daniel Jorge <djpgonzaga@gmail.com>
2022-08-18 09:47:36 -0300

    feat: empty menu selections page

--- app/routes/__index/menu/index.tsx
+++ app/routes/__index/menu/index.tsx
@@ -1,63 +1,14 @@
-import { Link, useLoaderData } from "@remix-run/react";
-import type { LoaderArgs } from "@remix-run/server-runtime";
-import { json } from "@remix-run/server-runtime";
-import { prisma } from "~/server/db.server";
-import { requireUserId } from "~/server/session.server";
+import { Link } from "@remix-run/react";
+import { Text } from "~/components";
 
-export async function loader({ request, params }: LoaderArgs) {
-  const userId = await requireUserId(request);
-  const menu = await prisma.menu.findFirst({
-    include: {
-      recipes: {
-        include: {
-          ingredients: {
-            include: {
-              ingredient: true,
-            },
-          },
-        },
-      },
-    },
-    where: { userId },
-  });
-  return json({ menu });
-}
-
-export default function Recipes() {
-  const data = useLoaderData<typeof loader>();
-
+export default function Menus() {
   return (
-    <div className="grid gap-4">
-      {data.menu ? (
-        <>
-          <h3 className="text-2xl font-bold">{data.menu.name}</h3>
-          <p>{data.menu.description}</p>
-          <ul className="grid gap-4">
-            {data.menu.recipes.map((a) => (
-              <li key={a.id} className="">
-                <p>{a.name}</p>
-                <p>{a.description}</p>
-                <div>
-                  {a.ingredients.map((b) => (
-                    <div key={b.id} className="flex gap-2">
-                      <p>{b.ingredient.name}</p>
-                      <p>{b.amount}</p>
-                      <p>{b.ingredient.unit}</p>
-                    </div>
-                  ))}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>
-          No recipes selected. Select a recipe on the left, or{" "}
-          <Link to="new" className="text-blue-500 underline">
-            create a new recipe.
-          </Link>
-        </p>
-      )}
-    </div>
+    <Text as="p" size="sm" weight="normal" color="dark">
+      No menu selected. Select a menu on the top, or{" "}
+      {/* @TODO Link component */}
+      <Link to="new" className={`transform text-sm font-medium text-blue-500`}>
+        create a new menu.
+      </Link>
+    </Text>
   );
 }
