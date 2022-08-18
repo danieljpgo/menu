5ba9dd195d94c7031f173f25b78ab3b11b825b65
Daniel Jorge <djpgonzaga@gmail.com>
2022-08-18 09:44:04 -0300

    feat: text component and gap for stack and shelf component

--- app/components/Stack.tsx
+++ app/components/Stack.tsx
@@ -1,13 +1,20 @@
 // @TODO: arial label for ul
+const gaps = {
+  sm: "gap-2",
+  md: "gap-4",
+  lg: "gap-6",
+  xl: "gap-8",
+};
 
 type StackProps = {
   children: React.ReactNode;
-  as?: "section" | "div" | "ul" | "li";
+  gap?: keyof typeof gaps;
+  as?: "section" | "div" | "ul" | "ol" | "li";
 };
 
 export default function Stack(props: StackProps) {
-  const { children, as: tag = "div" } = props;
+  const { children, as: tag = "div", gap = "sm" } = props;
   const Tag = tag;
 
-  return <Tag className="flex flex-col">{children}</Tag>;
+  return <Tag className={`flex flex-col ${gaps[gap]}`}>{children}</Tag>;
 }
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,15 @@
 export { default as Shelf } from "./Shelf";
 export { default as Stack } from "./Stack";
 export { default as Button } from "./Button";
+export { default as Text } from "./Text";
 export { default as Heading } from "./Heading";
 export { default as Container } from "./Container";
 export { default as Spinner } from "./Spinner";
 export { default as NavLink } from "./NavLink";
+export { default as Input } from "./Input";
+export { default as Label } from "./Label";
+export { default as TextField } from "./TextField";
+export { default as Select } from "./Select";
+export { default as SelectField } from "./SelectField";
+
+// export { default as Center } from "./Center";
