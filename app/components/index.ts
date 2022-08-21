47b3cbbe2bde6d6d272912e1ae14477fda08ab1c
Daniel Jorge <djpgonzaga@gmail.com>
2022-08-21 18:15:54 -0300

    fix: export number field component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,15 +1,16 @@
 export { default as Shelf } from "./Shelf";
 export { default as Stack } from "./Stack";
+export { default as Spinner } from "./Spinner";
+export { default as Container } from "./Container";
 export { default as Button } from "./Button";
 export { default as Text } from "./Text";
 export { default as Heading } from "./Heading";
-export { default as Container } from "./Container";
-export { default as Spinner } from "./Spinner";
 export { default as NavLink } from "./NavLink";
-export { default as Input } from "./Input";
 export { default as Label } from "./Label";
-export { default as TextField } from "./TextField";
+export { default as Input } from "./Input";
 export { default as Select } from "./Select";
+export { default as TextField } from "./TextField";
 export { default as SelectField } from "./SelectField";
+export { default as NumberField } from "./NumberField";
 
 // export { default as Center } from "./Center";
