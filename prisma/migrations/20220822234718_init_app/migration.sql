a9a38ca5ddc25efb79b23bd3f9cf1e6f632d103f
Daniel Jorge <djpgonzaga@gmail.com>
2022-08-22 20:47:51 -0300

    chore: db migrations again

--- prisma/migrations/20220822234718_init_app/migration.sql
+++ prisma/migrations/20220822234718_init_app/migration.sql
@@ -1,11 +1,18 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
 
-  - You are about to drop the column `menuId` on the `Recipe` table. All the data in the column will be lost.
-  - You are about to alter the column `amount` on the `RecipeIngredient` table. The data in that column could be lost. The data in that column will be cast from `Int` to `Float`.
-  - Made the column `recipeId` on table `RecipeIngredient` required. This step will fail if there are existing NULL values in that column.
+-- CreateTable
+CREATE TABLE "Password" (
+    "hash" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-*/
 -- CreateTable
 CREATE TABLE "Shop" (
     "id" TEXT NOT NULL PRIMARY KEY,
@@ -28,16 +35,7 @@ CREATE TABLE "PurchaseIngredient" (
 );
 
 -- CreateTable
-CREATE TABLE "_MenuToRecipe" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL,
-    CONSTRAINT "_MenuToRecipe_A_fkey" FOREIGN KEY ("A") REFERENCES "Menu" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "_MenuToRecipe_B_fkey" FOREIGN KEY ("B") REFERENCES "Recipe" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- RedefineTables
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_Menu" (
+CREATE TABLE "Menu" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
@@ -48,10 +46,9 @@ CREATE TABLE "new_Menu" (
     CONSTRAINT "Menu_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Menu_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-INSERT INTO "new_Menu" ("createdAt", "description", "id", "name", "updatedAt", "userId") SELECT "createdAt", "description", "id", "name", "updatedAt", "userId" FROM "Menu";
-DROP TABLE "Menu";
-ALTER TABLE "new_Menu" RENAME TO "Menu";
-CREATE TABLE "new_Recipe" (
+
+-- CreateTable
+CREATE TABLE "Recipe" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
@@ -60,10 +57,9 @@ CREATE TABLE "new_Recipe" (
     "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "Recipe_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO "new_Recipe" ("createdAt", "description", "id", "name", "updatedAt", "userId") SELECT "createdAt", "description", "id", "name", "updatedAt", "userId" FROM "Recipe";
-DROP TABLE "Recipe";
-ALTER TABLE "new_Recipe" RENAME TO "Recipe";
-CREATE TABLE "new_RecipeIngredient" (
+
+-- CreateTable
+CREATE TABLE "RecipeIngredient" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "amount" REAL NOT NULL,
     "recipeId" TEXT NOT NULL,
@@ -73,11 +69,38 @@ CREATE TABLE "new_RecipeIngredient" (
     CONSTRAINT "RecipeIngredient_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "RecipeIngredient_ingredientId_fkey" FOREIGN KEY ("ingredientId") REFERENCES "Ingredient" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO "new_RecipeIngredient" ("amount", "createdAt", "id", "ingredientId", "recipeId", "updatedAt") SELECT "amount", "createdAt", "id", "ingredientId", "recipeId", "updatedAt" FROM "RecipeIngredient";
-DROP TABLE "RecipeIngredient";
-ALTER TABLE "new_RecipeIngredient" RENAME TO "RecipeIngredient";
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
+
+-- CreateTable
+CREATE TABLE "Ingredient" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "unit" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_MenuToRecipe" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_MenuToRecipe_A_fkey" FOREIGN KEY ("A") REFERENCES "Menu" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_MenuToRecipe_B_fkey" FOREIGN KEY ("B") REFERENCES "Recipe" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Shop_userId_key" ON "Shop"("userId");
