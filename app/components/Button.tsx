b29dd06b90ec9e84155a9924e1c04aebe4d5a95d
Daniel Jorge <djpgonzaga@gmail.com>
2022-08-23 01:31:17 -0300

    feat: better markup for shop page

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,6 +8,7 @@ const sizes = {
 
 type ButtonProps = {
   children: string;
+  fill?: boolean;
   disabled?: React.ButtonHTMLAttributes<HTMLButtonElement>["disabled"];
   loading?: boolean;
   name?: React.ButtonHTMLAttributes<HTMLButtonElement>["name"];
@@ -20,6 +21,7 @@ type ButtonProps = {
 export default function Button(props: ButtonProps) {
   const {
     children,
+    fill,
     type = "button",
     name,
     value,
@@ -36,8 +38,9 @@ export default function Button(props: ButtonProps) {
       type={type}
       disabled={disabled || loading}
       className={`
-        w-full transform rounded-lg bg-gray-800 px-4 py-2 tracking-wide text-white transition-colors duration-200 hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-800 disabled:cursor-not-allowed disabled:bg-opacity-70
+        transform rounded-lg bg-gray-800 px-4 py-2 tracking-wide text-white transition-colors duration-200 hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-800 disabled:cursor-not-allowed disabled:bg-opacity-70
         ${sizes[size]}
+        ${fill ? "w-full" : ""}
     `}
       onClick={onClick}
     >
--- app/routes/__index/shop/index.tsx
+++ app/routes/__index/shop/index.tsx
@@ -1,14 +1,24 @@
 import { Form, Link, useLoaderData } from "@remix-run/react";
-import type { ActionArgs, LoaderArgs } from "@remix-run/server-runtime";
+import type {
+  ActionArgs,
+  LoaderArgs,
+  MetaFunction,
+} from "@remix-run/server-runtime";
 import { json, redirect } from "@remix-run/server-runtime";
 import { z } from "zod";
 import { Button, Heading, Input, Shelf, Stack, Text } from "~/components";
 import { prisma } from "~/server/db.server";
 import { requireUserId } from "~/server/session.server";
 
+export const meta: MetaFunction = () => ({
+  title: "Menu - Shop",
+});
+
 const schema = z.object({
   shopId: z.string({ required_error: "shopId not found" }),
-  method: z.string({ required_error: "method not found" }),
+  action: z.enum(["save", "delete", "edit"], {
+    required_error: "action not found",
+  }),
 });
 
 export async function loader({ request }: LoaderArgs) {
@@ -17,26 +27,18 @@ export async function loader({ request }: LoaderArgs) {
     where: { userId },
     select: {
       id: true,
+      purchaseIngredients: {
+        select: { id: true, bought: true, ingredient: true },
+      },
       menus: {
         select: {
           recipes: {
             select: {
               ingredients: {
-                select: {
-                  amount: true,
-                  id: true,
-                  ingredient: true,
-                },
+                select: { id: true, amount: true, ingredient: true },
               },
             },
           },
-        },
-      },
-      purchaseIngredients: {
-        select: {
-          id: true,
-          bought: true,
-          ingredient: true,
         },
       },
     },
@@ -46,98 +48,69 @@ export async function loader({ request }: LoaderArgs) {
     return json({ shop });
   }
 
-  const cde = shop.menus
-    .map((menu) => {
-      return menu.recipes.map((recipe) => {
-        return recipe.ingredients.map((ingredient) => {
-          return {
-            amount: ingredient.amount,
-            ingredientId: ingredient.ingredient.id,
-          };
-        });
-      });
-    })
-    .flatMap((a) => a.flatMap((b) => b));
+  const ingredients = shop.menus
+    .map((menu) =>
+      menu.recipes.map((recipe) =>
+        recipe.ingredients.map((ingredient) => ({
+          id: ingredient.ingredient.id,
+          amount: ingredient.amount,
+        }))
+      )
+    )
+    .flatMap((recipe) => recipe)
+    .flatMap((recipe) => recipe);
 
-  const calc = shop.purchaseIngredients.map((purchase) => {
-    const some = cde.filter(
-      (ingredient) => ingredient.ingredientId === purchase.ingredient.id
-    );
+  const purchases = shop.purchaseIngredients.map((purchase) => ({
+    ...purchase,
+    value: ingredients
+      .filter((ingredient) => ingredient.id === purchase.ingredient.id)
+      .reduce((acc, curr) => acc + curr.amount, 0),
+  }));
 
-    return {
-      ...purchase,
-      value: some.reduce((acc, curr) => acc + curr.amount, 0),
-    };
-  });
-
-  // const abc = shop?.purchaseIngredients.reduce((acc, curr, index) => {
-  //   // curr.ingredient.id
-  //   const cde = shop.menus.map((menu) => {
-  //     return menu.recipes.map(recipe => {
-  //       return recipe.ingredients.map(ingredient => {
-  //         return {
-  //           amount: ingredient.amount,
-  //           ingredientId: ingredient.ingredient.id
-  //         }
-  //       })
-  //     })
-
-  //     return acc1
-  //   }, [] as any)
-
-  //   return acc
-  // }, [] as any)
-
-  return json({
-    shop: {
-      id: shop.id,
-      ingredients: calc,
-    },
-  });
+  return json({ shop: { id: shop.id, purchases } });
 }
 
 export async function action({ request }: ActionArgs) {
   const userId = await requireUserId(request);
   const formData = await request.formData();
-
   const validation = schema.safeParse({
     shopId: formData.get("shopId"),
-    method: formData.get("_method"),
+    action: formData.get("action"),
   });
 
+  // @TODO better error handler
   if (!validation.success) {
-    const { message } = validation.error.issues[0];
-    throw new Error(message);
+    throw new Error(validation.error.issues[0].message);
   }
+  const form = validation.data;
 
-  if (validation.data.method === "delete") {
+  if (form.action === "delete") {
     await prisma.shop.deleteMany({
       where: { id: validation.data.shopId, userId },
     });
+    return json({ ok: true, action: form.action });
   }
 
-  if (validation.data.method === "save") {
-    const boughts = formData.getAll("ingredientId").map((a) => ({
-      id: a,
-      check: formData.getAll(`bought - ${a}`),
-    }));
+  if (form.action === "save") {
+    const boughts = formData
+      .getAll("ingredientId")
+      .map((id) => ({ id, check: formData.getAll(`bought-${id}`) }));
+
     await prisma.shop.update({
-      where: {
-        id: validation.data.shopId,
-      },
+      where: { id: validation.data.shopId },
       data: {
         purchaseIngredients: {
           updateMany: boughts.map((bought) => ({
             where: { id: String(bought.id) },
-            data: {
-              bought: bought.check.includes("on"),
-            },
+            data: { bought: bought.check.includes("on") },
           })),
         },
       },
     });
+    return json({ ok: true, action: form.action });
   }
-  return redirect("/shop");
+
+  return redirect(".");
 }
 
 export default function Shop() {
@@ -156,70 +129,64 @@ export default function Shop() {
 
   return (
     <>
-      <Form method="post">
-        <Input type="hidden" name="shopId" value={data.shop?.id} />
-        <Stack gap="md">
+      <Stack gap="md">
+        <Form method="post">
+          <Input type="hidden" name="shopId" value={data.shop.id} />
           <div className="flex items-center justify-between">
             <Heading as="h3" weight="medium">
               Ingredient
             </Heading>
-            <div>
-              <Button size="sm" type="submit" name="_method" value="save">
-                save
-              </Button>
-            </div>
+            <Button size="sm" type="submit" name="action" value="save">
+              save
+            </Button>
           </div>
           <Stack as="ul" gap="xs">
-            {data.shop?.ingredients.map((ingredient, index) => (
+            {data.shop?.purchases.map((purchase) => (
               <li
-                key={ingredient.ingredient.id}
-                className="flex items-center justify-between"
+                key={purchase.ingredient.id}
+                className="grid items-center justify-between grid-flow-col"
               >
                 <Shelf>
-                  <Text>{ingredient.ingredient.name}</Text>-
+                  <Text>{purchase.ingredient.name}</Text>-
                   <Text color="light">
-                    {ingredient.ingredient.unit === "p"
-                      ? ingredient.value
-                      : `${ingredient.value} ${ingredient.ingredient.unit}`}
+                    {purchase.ingredient.unit === "p"
+                      ? purchase.value
+                      : `${purchase.value} ${purchase.ingredient.unit}`}
                   </Text>
                 </Shelf>
-                <div>
-                  <Input
-                    type="hidden"
-                    name="ingredientId"
-                    value={ingredient.id}
-                  />
-                  <Input
-                    type="hidden"
-                    name={`bought - ${ingredient.id}`}
-                    value="off"
-                  />
-                  <Input
-                    type="checkbox"
-                    name={`bought - ${ingredient.id}`}
-                    defaultChecked={ingredient.bought}
-                  />
-                </div>
+                <Input type="hidden" name="ingredientId" value={purchase.id} />
+                <Input
+                  type="hidden"
+                  name={`bought-${purchase.id}`}
+                  value="off"
+                />
+                <Input
+                  type="checkbox"
+                  name={`bought-${purchase.id}`}
+                  defaultChecked={purchase.bought}
+                />
               </li>
             ))}
           </Stack>
-        </Stack>
-      </Form>
-      <div className="fixed bottom-0 left-0 right-0 grid gap-4 px-6 pb-4 bg-white ">
+        </Form>
+      </Stack>
+      <div className="fixed bottom-0 left-0 right-0 grid gap-4 px-6 pb-4 bg-white">
         <hr />
-        <div className="flex justify-between">
-          <Form method="post">
-            <Button size="sm" type="submit" name="_method" value="delete">
-              Delete
-            </Button>
-          </Form>
-          <div>
-            <Button size="sm" type="button" disabled>
-              edit
-            </Button>
-          </div>
-        </div>
+        <Form method="post" className="flex justify-between">
+          <Input type="hidden" name="shopId" value={data.shop.id} />
+          <Button size="sm" type="submit" name="action" value="delete">
+            Delete
+          </Button>
+          <Button size="sm" type="button" name="action" value="edit">
+            edit
+          </Button>
+        </Form>
       </div>
     </>
   );
 }
+
+// @TODO useFetch for saving?
+// @TODO catch boundary
+// @TODO error boundary
+// @TODO form error handle
