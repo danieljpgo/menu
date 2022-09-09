8f2e904e4476ff485990778905f6327d74cfa65f
Daniel Jorge <djpgonzaga@gmail.com>
2022-09-09 00:28:52 -0300

    feat: remove zoon

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  viewport: "width=device-width,initial-scale=1",
+  viewport: "width=device-width,initial-scale=1,user-scalable=no",
   ...pwaMetas,
 });
 
